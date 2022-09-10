ea41e8d60d35df8c99b4077417e3a209e4841d15
Daivison Morais <daivison13pinheiro@gmail.com>
2022-09-10 15:02:11 -0300

    hashing implementado

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
@@ -22,7 +24,7 @@ const postRegistration = joi.object({
   password: joi.string().required(),
 });
 
-app.post("/registration", async (req, res) => {
+app.post("/sign-up", async (req, res) => {
   const { email, name, foto, password } = req.body;
 
   const validation = postRegistration.validate(req.body, { abortEarly: false });
@@ -31,15 +33,9 @@ app.post("/registration", async (req, res) => {
     const erros = validation.error.details.map((value) => value.message);
     return res.status(442).send(erros);
   }
+  const passwordEncrypted = bcrypt.hashSync(password, 10);
 
   try {
-    const findName = await db
-      .collection("registration")
-      .findOne({ name: name });
-    if (findName) {
-      return res.status(409).send({ error: "Este nome já está em uso" });
-    }
-
     const findEmail = await db
       .collection("registration")
       .findOne({ email: email });
@@ -47,12 +43,34 @@ app.post("/registration", async (req, res) => {
       return res.status(409).send({ error: "Este email já está em uso" });
     }
 
-    await db.collection("registration").insertOne(req.body);
+    await db.collection("registration").insertOne({
+      name,
+      email,
+      foto,
+      password: passwordEncrypted,
+    });
     res.send("201");
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
   }
+});
+
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  const findEmail = await db
+    .collection("registration")
+    .findOne({ email: email });
+  if (!findEmail) {
+    return res.status(409).send({ error: "Usuário não encontrado" });
+  }
+
+  const isValid = bcrypt.compareSync(password, findEmail.password);
+  if (!isValid) {
+    return res.status(401).send({ error: "Senha ou email, errado" });
+  }
+  res.send(200);
 });
 
 app.listen(5000, () => {

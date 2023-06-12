487cdd6bdd5abd424e66cb8fd4e61e408de11428
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-06-11 22:22:07 -0300

    refact: pequenas correções aplicada

--- front-end/src/components/services.js
+++ front-end/src/components/services.js
@@ -1,6 +1,5 @@
 
-const BASE_URL="http://localhost:5000";
+const BASE_URL="http://localhost:5000" | "https://wallet-w8pz.onrender.com";
 
 export default BASE_URL;
 
-//https://wallet-w8pz.onrender.com

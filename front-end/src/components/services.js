f4e4e1cdefa3d3d9ad35e22d94105cadd2f021a4
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-09-01 22:31:55 -0300

    testes para deploy retirados e mongoDB atlas agora é utilizado

--- front-end/src/components/services.js
+++ front-end/src/components/services.js
@@ -1,5 +1,5 @@
 
-const BASE_URL="http://localhost:8081/api";
+const BASE_URL="http://localhost:5000";
 //"http://localhost:5000"
 //"https://wallet-w8pz.onrender.com"
 //"http://localhost:8081/api"

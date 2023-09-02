911aa7678b4d045c88f5913265a646abd3caf98f
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-09-01 22:50:26 -0300

    para fazer deploy

--- front-end/src/components/services.js
+++ front-end/src/components/services.js
@@ -1,8 +1,5 @@
-
-const BASE_URL="http://localhost:5000";
-//"http://localhost:5000"
-//"https://wallet-w8pz.onrender.com"
-//"http://localhost:8081/api"
+const BASE_URL = "https://api-wallet-38qm.onrender.com"; //-> deploy com render
+//"http://localhost:5000" -> para uso sem docker
+//"http://localhost:8081/api" -> para uso com docker
 
 export default BASE_URL;
-

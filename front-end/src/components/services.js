1cf3af3cda21f40b2ff419958beaadf03e93504a
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-09-19 08:37:14 -0300

    alterações para deploy

--- front-end/src/components/services.js
+++ front-end/src/components/services.js
@@ -1,5 +1,5 @@
-//const BASE_URL = "https://api-wallet-38qm.onrender.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://api-wallet-38qm.onrender.com";
+//const BASE_URL = "http://localhost:5000";
 //const BASE_URL = "http://localhost:8081/api";
 
 export default BASE_URL;
